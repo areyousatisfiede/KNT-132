9aa268780a3bd7f5f044b878ad4df9e438c888f0
areyousatisfiede <130303748+areyousatisfiede@users.noreply.github.com>
2023-04-09 19:45:39 +0300

    changed variable name!

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,21 +5,21 @@ using namespace std;
 class Delta
 {
 private:
-    int descriptor;
+    int desc;
 public:
     Delta() {
-        static int next_descriptor = 1;
-        descriptor = next_descriptor++;
+        static int next_desc = 1;
+        desc = next_desc++;
     }
-    int returnDescriptor() {
-        return descriptor;
+    int returndesc() {
+        return desc;
     }
 };
 
 int main()
 {
     Delta d1, d2, d3;
-    cout << dye::red("Number One: ") << dye::white_on_red(d1.returnDescriptor()) << endl;
-    cout << dye::aqua("Number Two: ") << dye::white_on_aqua(d2.returnDescriptor()) << endl;
-    cout << dye::yellow("Number Three: ") << dye::white_on_yellow(d3.returnDescriptor()) << endl;
+    cout << dye::red("Number One: ") << dye::white_on_red(d1.returndesc()) << endl;
+    cout << dye::aqua("Number Two: ") << dye::white_on_aqua(d2.returndesc()) << endl;
+    cout << dye::yellow("Number Three: ") << dye::white_on_yellow(d3.returndesc()) << endl;
 }
