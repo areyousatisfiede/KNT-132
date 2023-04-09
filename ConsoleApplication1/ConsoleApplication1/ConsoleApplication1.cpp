373a3de766bd6a64432c508f4badd5372e978f4f
areyousatisfiede <130303748+areyousatisfiede@users.noreply.github.com>
2023-04-09 19:39:04 +0300

    created initial commit!

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "color.hpp"
+using namespace std;
+
+class Delta
+{
+private:
+    int descriptor;
+public:
+    Delta() {
+        static int next_descriptor = 1;
+        descriptor = next_descriptor++;
+    }
+    int returnDescriptor() {
+        return descriptor;
+    }
+};
+
+int main()
+{
+    Delta d1, d2, d3;
+    cout << dye::red("Number One: ") << dye::white_on_red(d1.returnDescriptor()) << endl;
+    cout << dye::aqua("Number Two: ") << dye::white_on_aqua(d2.returnDescriptor()) << endl;
+    cout << dye::yellow("Number Three: ") << dye::white_on_yellow(d3.returnDescriptor()) << endl;
+}
